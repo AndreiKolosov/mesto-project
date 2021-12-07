98356d8530637b730605f86fc14f01b675221e39
Andrei Kolosov <Oldliberty@yandex.ru>
2021-12-07 21:52:06 +0200

    feat: function for new cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,113 @@
-// Находим окно формы и объявляем переменную
-let editProfile = document.querySelector('.popup');
-// Создаем функцию-обработчик, добавляющую класс окну формы
-function openPopup() {
-  editProfile.classList.add('popup_opened');
+// Находим popup редактирования профиля**************************************************************************************************
+const editProfilePopup = document.querySelector('.popup_type_profile-editor');
+// Создаем функцию-обработчик, добавляющую класс для popup
+function openEditorPopup() {
+  editProfilePopup.classList.add('popup_opened');
+}
+// Убирающую класс
+function closeEditorPopup() {
+  editProfilePopup.classList.remove('popup_opened');
+  cardAdderPopup.classList.remove('popup_opened');
 }
 
-// Находим кнопку открытия формы редактирования профиля и объявляем переменную
-let profileEditBtn = document.querySelector('.profile__edit-button');
-let cardAddBtn = document.querySelector('.profile__add-button');
-// Вешаем слушатель события clik на кнопку редактирования и
-profileEditBtn.addEventListener('click', openPopup);
+// Находим кнопку открытия окна редактирования профиля и объявляем переменную
+const profileEditBtn = document.querySelector('.profile__edit-button');
+// Вешаем слушатель clik на кнопку редактирования
+profileEditBtn.addEventListener('click', openEditorPopup);
 
-// Находим кнопку закрытия формы редактирования профиля и объявляем переменную
-let closePopupBtn = document.querySelector('.popup__close-button');
-// Тоже, что и сверху, только функция-обработчик события внутри.
-// Запись выглядит немного короче
-closePopupBtn.addEventListener('click', function () {
-  editProfile.classList.remove('popup_opened');
-});
+// Находим кнопку закрытия окна редактирования профиля и объявляем переменную
+const closePopupBtn = document.querySelector('.popup__close-button');
+// Прикрепляем слушатель
+closePopupBtn.addEventListener('click', closeEditorPopup);
 
-// Реализую редактирование профиля
-// Выбираю форму
-const formElement = document.querySelector('.form');
-// Поля ввода
-const nameInput = document.querySelector('#user-name');
-const descriptionInput = document.querySelector('#user-description');
-//  Выбираю элементы, которые буду менять с помощью value
+// Popup добавления карточки
+const cardAdderPopup = document.querySelector('.popup_type_card_adder');
+
+function openAdderPopup() {
+  cardAdderPopup.classList.add('popup_opened');
+}
+
+function closeAdderPopup() {
+  cardAdderPopup.classList.remove('popup_opened');
+}
+
+const addCardBtn = document.querySelector('.profile__add-button');
+addCardBtn.addEventListener('click', openAdderPopup);
+
+// Реализую редактирование профиля *************************************************************************************
+//  Выбираю элементы имени и описания на странице
 const userName = document.querySelector('.profile__name');
 const userDescription = document.querySelector('.profile__description');
+// Поля ввода
+let nameInput = document.querySelector('#user-name');
+let descriptionInput = document.querySelector('#user-description');
+// Выбираю форму
+const formElement = document.querySelector('.form');
 //  Кнопка сохранить
 const saveEdits = document.querySelector('.form__save-button');
+
+nameInput.value = userName.textContent;
+descriptionInput.value = userDescription.textContent;
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
-
-  // При клике на кнопку сохранить удаляется класс и закрывается модальное окно
-  saveEdits.addEventListener('click', function () {
-    editProfile.classList.remove('popup_opened');
-  });
+  closePopup();
 }
 
-//
 formElement.addEventListener('submit', formSubmitHandler);
-// Кнопка лайка
-const likeBtn = document.querySelector('.card__like-button');
-likeBtn.addEventListener('click', function (event) {
-  event.target.classList.toggle('card__like-button_active');
-});
+
+// Добавление карточки****************************************************************************************************
+const galleryContainer = document.querySelector('.gallery__list'); // Контейнер карточек
+const cardTemplate = document.querySelector('#card-template'); // шаблон карточки
+// Массив для карточек "из коробки"
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+function addCard(place) {
+  const cardElement = document.querySelector('.card').cloneNode(true); // карточка полученная из шаблона
+  const removeCardBtn = cardElement.querySelector('.card__trash-button'); // кнопкаудаления карточки
+  const likeCardBtn = cardElement.querySelector('.card__like-button'); // кнопка лайк
+
+  function likeCard() {
+    likeCardBtn.classList.toggle('card__like-button_active');
+  }
+
+  likeCardBtn.addEventListener('click', likeCard);
+  removeCardBtn.addEventListener('click', removeCard);
+
+  cardElement.querySelector('.card__name').textContent = place.name;
+  cardElement.querySelector('.card__image').alt = plase.name;
+  cardElement.querySelector('.card__image').src = place.link;
+
+  return cardElement;
+}
+
+function removeCard(event) {
+  event.target.closest('.card').remove();
+}
