68f71c44c6fa82263877fe85b36e35484d7a3ea2
Andrei Kolosov <Oldliberty@yandex.ru>
2021-12-09 18:19:42 +0200

    feat: add initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,20 @@
-// Находим popup редактирования профиля**************************************************************************************************
-const editProfilePopup = document.querySelector('.popup_type_profile-editor');
-// Создаем функцию-обработчик, добавляющую класс для popup
-function openEditorPopup() {
-  editProfilePopup.classList.add('popup_opened');
-}
-// Убирающую класс
-function closeEditorPopup() {
-  editProfilePopup.classList.remove('popup_opened');
-  cardAdderPopup.classList.remove('popup_opened');
-}
-
-// Находим кнопку открытия окна редактирования профиля и объявляем переменную
-const profileEditBtn = document.querySelector('.profile__edit-button');
-// Вешаем слушатель clik на кнопку редактирования
-profileEditBtn.addEventListener('click', openEditorPopup);
-
-// Находим кнопку закрытия окна редактирования профиля и объявляем переменную
-const closePopupBtn = document.querySelector('.popup__close-button');
-// Прикрепляем слушатель
-closePopupBtn.addEventListener('click', closeEditorPopup);
-
-// Popup добавления карточки
-const cardAdderPopup = document.querySelector('.popup_type_card_adder');
-
-function openAdderPopup() {
-  cardAdderPopup.classList.add('popup_opened');
-}
-
-function closeAdderPopup() {
-  cardAdderPopup.classList.remove('popup_opened');
-}
-
-const addCardBtn = document.querySelector('.profile__add-button');
-addCardBtn.addEventListener('click', openAdderPopup);
-
-// Реализую редактирование профиля *************************************************************************************
-//  Выбираю элементы имени и описания на странице
-const userName = document.querySelector('.profile__name');
-const userDescription = document.querySelector('.profile__description');
-// Поля ввода
-let nameInput = document.querySelector('#user-name');
-let descriptionInput = document.querySelector('#user-description');
-// Выбираю форму
-const formElement = document.querySelector('.form');
-//  Кнопка сохранить
-const saveEdits = document.querySelector('.form__save-button');
-
-nameInput.value = userName.textContent;
-descriptionInput.value = userDescription.textContent;
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userDescription.textContent = descriptionInput.value;
-  closePopup();
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-// Добавление карточки****************************************************************************************************
-const galleryContainer = document.querySelector('.gallery__list'); // Контейнер карточек
-const cardTemplate = document.querySelector('#card-template'); // шаблон карточки
-// Массив для карточек "из коробки"
+const editorPopup = document.querySelector('.popup_type_profile-editor'); // Окно редактирования профиля
+const cardAdderPopup = document.querySelector('.popup_type_card_adder'); // Окно добавления карточки
+const imagePopup = document.querySelector('.popup_type_img'); // Окно просмотра фотографии
+const editBtn = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
+const addBtn = document.querySelector('.profile__add-button'); // Кнопка добавления карточки
+const closeBtnInEditor = editorPopup.querySelector('.popup__close-button'); // Кнопка закрытия редактора профиля
+const closeBtnInCardAdder = cardAdderPopup.querySelector(
+  '.popup__close-button'
+); // Кнопка закрытия окна добавления карточки
+const closeBtnImgPopup = imagePopup.querySelector('.popup__close-button');
+const userName = document.querySelector('.profile__name'); // Имя пользователя на странице
+const userDescription = document.querySelector('.profile__description'); // Описание пользователя на странице
+let nameInput = document.querySelector('#user-name'); // Поле ввода формы редактирования
+let descriptionInput = document.querySelector('#user-description'); // Поле ввода формы редактирования
+const formElement = document.querySelector('.form'); // Форма редактирования
+const galleryContainer = document.querySelector('.galery__list'); // Контейнер карточек
+const cardTemplate = document.querySelector('.card-template').content; // Шаблон карточки
 const initialCards = [
   {
     name: 'Архыз',
@@ -87,27 +40,58 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
   },
-];
+]; // Массив карточек "из коробки"
 
-function addCard(place) {
-  const cardElement = document.querySelector('.card').cloneNode(true); // карточка полученная из шаблона
-  const removeCardBtn = cardElement.querySelector('.card__trash-button'); // кнопкаудаления карточки
-  const likeCardBtn = cardElement.querySelector('.card__like-button'); // кнопка лайк
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+} // Функция открытия модально окна принимающая на вход переменную нужного модального окна
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+nameInput.value = userName.textContent;
+descriptionInput.value = userDescription.textContent;
+
+function formEditorSubmitHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userDescription.textContent = descriptionInput.value;
+  closePopup(editorPopup);
+} // Форма редактирования профиля
+
+function createCard(name, link) {
+  const createdCard = cardTemplate.querySelector('.card').cloneNode(true); // Карточка, склонированная из шаблона
+  const removeCardBtn = createdCard.querySelector('.card__trash-button'); // Кнопка удаления карточки
+  const likeBtn = createdCard.querySelector('.card__like-button');
+  const cardName = createdCard.querySelector('.card__name');
+  const cardImg = createdCard.querySelector('.card__image');
+
+  cardName.textContent = name;
+  cardImg.src = link;
+  cardImg.alt = name;
 
   function likeCard() {
-    likeCardBtn.classList.toggle('card__like-button_active');
-  }
+    likeBtn.classList.toggle('card__like-button_active');
+  } // Лайк
 
-  likeCardBtn.addEventListener('click', likeCard);
+  function removeCard(event) {
+    event.target.closest('.card').remove();
+  } // Удаление карточки
+
+  likeBtn.addEventListener('click', likeCard);
   removeCardBtn.addEventListener('click', removeCard);
 
-  cardElement.querySelector('.card__name').textContent = place.name;
-  cardElement.querySelector('.card__image').alt = plase.name;
-  cardElement.querySelector('.card__image').src = place.link;
-
-  return cardElement;
+  return createdCard;
 }
 
-function removeCard(event) {
-  event.target.closest('.card').remove();
-}
+const newCards = initialCards.map(function (place) {
+  return createCard(place.name, place.link);
+}); // Перебираю массив
+galleryContainer.append(...newCards); // Вставляю карточки
+
+editBtn.addEventListener('click', () => openPopup(editorPopup));
+addBtn.addEventListener('click', () => openPopup(cardAdderPopup));
+closeBtnInEditor.addEventListener('click', () => closePopup(editorPopup));
+closeBtnInCardAdder.addEventListener('click', () => closePopup(cardAdderPopup));
+formElement.addEventListener('submit', formEditorSubmitHandler);
