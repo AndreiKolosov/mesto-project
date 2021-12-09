48e7686edd3b96db271c4cd73fd98e082df92c63
Andrei Kolosov <Oldliberty@yandex.ru>
2021-12-09 19:52:56 +0200

    feat: create function to add new cards

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,20 @@ const imagePopup = document.querySelector('.popup_type_img'); // Окно про
 const editBtn = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
 const addBtn = document.querySelector('.profile__add-button'); // Кнопка добавления карточки
 const closeBtnInEditor = editorPopup.querySelector('.popup__close-button'); // Кнопка закрытия редактора профиля
-const closeBtnInCardAdder = cardAdderPopup.querySelector(
-  '.popup__close-button'
-); // Кнопка закрытия окна добавления карточки
-const closeBtnImgPopup = imagePopup.querySelector('.popup__close-button');
+const closeBtnInAdder = cardAdderPopup.querySelector('.popup__close-button'); // Кнопка закрытия окна добавления карточки
+const closeBtnInImage = imagePopup.querySelector('.popup__close-button'); // Кнопка закрытия окна просмотра фотографии
 const userName = document.querySelector('.profile__name'); // Имя пользователя на странице
 const userDescription = document.querySelector('.profile__description'); // Описание пользователя на странице
-let nameInput = document.querySelector('#user-name'); // Поле ввода формы редактирования
-let descriptionInput = document.querySelector('#user-description'); // Поле ввода формы редактирования
-const formElement = document.querySelector('.form'); // Форма редактирования
+const editorFormElement = document.querySelector('#editor-form'); // Форма редактирования
+let nameInput = document.querySelector('#user-name'); // Поле ввода имени
+let descriptionInput = document.querySelector('#user-description'); // Поле ввода описания
+const adderFormElement = document.querySelector('#adder-form'); // Форма добавления карточки
+let placeNameInput = document.querySelector('#place-name'); // Поле ввода имени карточки
+let placeLinkInput = document.querySelector('#place-img-link'); // Поле ввода ссылки на фотографию
 const galleryContainer = document.querySelector('.galery__list'); // Контейнер карточек
 const cardTemplate = document.querySelector('.card-template').content; // Шаблон карточки
+const photoSizeBig = document.querySelector('.popup__image'); // Фото в модальном окне
+const photoCaption = document.querySelector('.popup__img-caption'); // Подпись к фото в модальном окне
 const initialCards = [
   {
     name: 'Архыз',
@@ -50,15 +53,25 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-nameInput.value = userName.textContent;
-descriptionInput.value = userDescription.textContent;
-
 function formEditorSubmitHandler(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
   closePopup(editorPopup);
 } // Форма редактирования профиля
+
+function formAdderSubmitHandler(evt) {
+  evt.preventDefault();
+
+  galleryContainer.prepend(
+    createCard(placeNameInput.value, placeLinkInput.value)
+  );
+
+  placeLinkInput.value = '';
+  placeNameInput.value = '';
+
+  closePopup(cardAdderPopup);
+}
 
 function createCard(name, link) {
   const createdCard = cardTemplate.querySelector('.card').cloneNode(true); // Карточка, склонированная из шаблона
@@ -79,8 +92,16 @@ function createCard(name, link) {
     event.target.closest('.card').remove();
   } // Удаление карточки
 
+  function expendPhoto() {
+    openPopup(imagePopup);
+    photoSizeBig.src = link;
+    photoSizeBig.alt = name;
+    photoCaption.textContent = name;
+  }
+
   likeBtn.addEventListener('click', likeCard);
   removeCardBtn.addEventListener('click', removeCard);
+  cardImg.addEventListener('click', expendPhoto);
 
   return createdCard;
 }
@@ -93,5 +114,7 @@ galleryContainer.append(...newCards); // Вставляю карточки
 editBtn.addEventListener('click', () => openPopup(editorPopup));
 addBtn.addEventListener('click', () => openPopup(cardAdderPopup));
 closeBtnInEditor.addEventListener('click', () => closePopup(editorPopup));
-closeBtnInCardAdder.addEventListener('click', () => closePopup(cardAdderPopup));
-formElement.addEventListener('submit', formEditorSubmitHandler);
+closeBtnInAdder.addEventListener('click', () => closePopup(cardAdderPopup));
+closeBtnInImage.addEventListener('click', () => closePopup(imagePopup));
+editorFormElement.addEventListener('submit', formEditorSubmitHandler);
+adderFormElement.addEventListener('submit', formAdderSubmitHandler);
