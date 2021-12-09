18748ab2d8a73ffad8c2ebe4f1efe264fea7e9fa
Andrei Kolosov <Oldliberty@yandex.ru>
2021-12-09 20:21:36 +0200

    refactor: put some more comments in script

--- scripts/index.js
+++ scripts/index.js
@@ -46,17 +46,17 @@ const initialCards = [
 ]; // Массив карточек "из коробки"
 
 function openPopup(popup) {
-  popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened'); // Добавляет класс
 } // Функция открытия модально окна принимающая на вход переменную нужного модального окна
 
 function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
+  popup.classList.remove('popup_opened'); // Убирает класс
+} // Функция закрытия модального окна
 
 function formEditorSubmitHandler(evt) {
   evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userDescription.textContent = descriptionInput.value;
+  userName.textContent = nameInput.value; // Значение value поля ввода === текстовому содержимому тега на странице
+  userDescription.textContent = descriptionInput.value; // Аналогично верхнему
   closePopup(editorPopup);
 } // Форма редактирования профиля
 
@@ -65,8 +65,9 @@ function formAdderSubmitHandler(evt) {
 
   galleryContainer.prepend(
     createCard(placeNameInput.value, placeLinkInput.value)
-  );
+  ); // Отправляю value импутов в качестве параметров фунции, создаю карточку, кладу в начало списка
 
+  // Делаю импуты пустыми после закрытия
   placeLinkInput.value = '';
   placeNameInput.value = '';
 
@@ -76,13 +77,13 @@ function formAdderSubmitHandler(evt) {
 function createCard(name, link) {
   const createdCard = cardTemplate.querySelector('.card').cloneNode(true); // Карточка, склонированная из шаблона
   const removeCardBtn = createdCard.querySelector('.card__trash-button'); // Кнопка удаления карточки
-  const likeBtn = createdCard.querySelector('.card__like-button');
-  const cardName = createdCard.querySelector('.card__name');
-  const cardImg = createdCard.querySelector('.card__image');
+  const likeBtn = createdCard.querySelector('.card__like-button'); // Кнопка лайк
+  const cardName = createdCard.querySelector('.card__name'); // Элемент с названием карточки
+  const cardImg = createdCard.querySelector('.card__image'); // Элемент Img в карточке
 
-  cardName.textContent = name;
-  cardImg.src = link;
-  cardImg.alt = name;
+  cardName.textContent = name; // Текстовое содержимое тега === параметру name
+  cardImg.src = link; // Атрибут src у img становится === параметру link
+  cardImg.alt = name; // Атрибут alt у img становится === параметру name
 
   function likeCard() {
     likeBtn.classList.toggle('card__like-button_active');
@@ -97,24 +98,24 @@ function createCard(name, link) {
     photoSizeBig.src = link;
     photoSizeBig.alt = name;
     photoCaption.textContent = name;
-  }
+  } // Развернуть окно просмотра карточки
 
-  likeBtn.addEventListener('click', likeCard);
-  removeCardBtn.addEventListener('click', removeCard);
-  cardImg.addEventListener('click', expendPhoto);
+  likeBtn.addEventListener('click', likeCard); // Отслеживаю клик по сердечку
+  removeCardBtn.addEventListener('click', removeCard); // Отслеживаю клик по корзине
+  cardImg.addEventListener('click', expendPhoto); // Отслеживаю клик по картинке
 
-  return createdCard;
+  return createdCard; // Возвращаю созданную карточку
 }
 
 const newCards = initialCards.map(function (place) {
-  return createCard(place.name, place.link);
+  return createCard(place.name, place.link); // На каждой итерации из массива берется объект и его ключи отправляются в функцию создания карточки и кладу в новый массив
 }); // Перебираю массив
-galleryContainer.append(...newCards); // Вставляю карточки
+galleryContainer.append(...newCards); // Вставляю карточки спредом
 
-editBtn.addEventListener('click', () => openPopup(editorPopup));
-addBtn.addEventListener('click', () => openPopup(cardAdderPopup));
-closeBtnInEditor.addEventListener('click', () => closePopup(editorPopup));
-closeBtnInAdder.addEventListener('click', () => closePopup(cardAdderPopup));
-closeBtnInImage.addEventListener('click', () => closePopup(imagePopup));
-editorFormElement.addEventListener('submit', formEditorSubmitHandler);
-adderFormElement.addEventListener('submit', formAdderSubmitHandler);
+editBtn.addEventListener('click', () => openPopup(editorPopup)); // Отслеживаю клик по кнопке редактирования
+addBtn.addEventListener('click', () => openPopup(cardAdderPopup)); // Отслеживаю клик по кнопки добавления
+closeBtnInEditor.addEventListener('click', () => closePopup(editorPopup)); // Клик по кнопки закрытия
+closeBtnInAdder.addEventListener('click', () => closePopup(cardAdderPopup)); // Клик по кнопки закрытия
+closeBtnInImage.addEventListener('click', () => closePopup(imagePopup)); // Клик по кнопки закрытия
+editorFormElement.addEventListener('submit', formEditorSubmitHandler); // Сабмит формы редактирования
+adderFormElement.addEventListener('submit', formAdderSubmitHandler); // Сабмит формы добавления
