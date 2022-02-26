17144815941565964505c0af573fd49bfe0807dc
Andrei Kolosov <Oldliberty@yandex.ru>
2022-02-26 17:16:56 +0200

    feat: connect to APi

---
+++ src/components/api.js
@@ -0,0 +1,30 @@
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort7',
+  headers: {
+    authorization: '32e69527-8018-4c7f-823e-614d86c48870',
+    'Content-Type': 'application/json',
+  },
+};
+
+function responseHandler(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+function errorHendler(err) {
+  console.log(err);
+}
+
+function getUser() {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then(responseHandler);
+}
+
+function getCards() {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then(responseHandler);
+}
